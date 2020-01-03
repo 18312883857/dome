ff0782d7f36502cc60bdac4096e853c81dbd19bd
砥砺前行 <1531012257@qq.com>
2020-01-03 23:50:25 +0800

    12312321

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'https://www.ele.me',
+                pathRewrite: { '^/api': '/' },
+                changeOrigin: true,     // target是域名的话，需要这个参数，
+                secure: false,          // 设置支持https协议的代理
+            },
+        }
+    }
+}
